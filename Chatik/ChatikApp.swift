07eaf2a91f190cbf038ff215bdf64f9df062f445
Kseniya <kislayakseniya@yandex.ru>
2026-09-19 18:50:27 +0300

    chore: initial commit

---
+++ Chatik/ChatikApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatikApp.swift
+//  Chatik
+//
+//  Created by Kseniya on 19.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChatikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chatik/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chatik
+//
+//  Created by Kseniya on 19.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
